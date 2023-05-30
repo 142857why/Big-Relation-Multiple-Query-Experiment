5ba55fd9c0303b17ef5146d8e217f81f30dc9db3
Rui Zhou <ruzhou@ethz.ch>
2023-05-31 00:05:47 +0200

    :art:

--- run_ex01.sh
+++ run_ex01.sh
@@ -19,8 +19,7 @@ g++ -O3 -Wall -Wno-unused-variable -std=c++17 \
 -include cpp/retailer/retailer_01_01.hpp \
 -include src/application/retailer/application_retailer.hpp \
 -o bin/retailer/retailer_01_01_BATCH && \
-./bin/retailer/retailer_01_01_BATCH > out/retailer_01_01_BATCH$BATCH_SIZE.out && \
-
+echo "Plan 1 compilation done, starting Compiling Plan 2 ..." && \
 ./run_backend.sh --batch -o cpp/retailer/retailer_01_02.hpp m3/retailer/retailer_01_02.m3 && \
 g++ -O3 -Wall -Wno-unused-variable -std=c++17 \
 -pedantic src/main.cpp \
@@ -31,4 +30,8 @@ g++ -O3 -Wall -Wno-unused-variable -std=c++17 \
 -include cpp/retailer/retailer_01_02.hpp \
 -include src/application/retailer/application_retailer.hpp \
 -o bin/retailer/retailer_01_02_BATCH && \
-./bin/retailer/retailer_01_02_BATCH > out/retailer_01_02_BATCH$BATCH_SIZE.out && \
+echo "Plan 2 compilation done, starting running Plan 1 ..." && \
+./bin/retailer/retailer_01_01_BATCH > out/retailer_01_01_BATCH$BATCH_SIZE.out && \
+echo "Plan 1 done, starting running Plan 2 ..." && \
+./bin/retailer/retailer_01_02_BATCH > out/retailer_01_02_BATCH$BATCH_SIZE.out
+echo "Plan 2 done, all done!"
